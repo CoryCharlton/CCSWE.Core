61a20fbe062e310dd2ed3dceb9b27d84511ab91a
Cory Charlton <ccharlton@pobox.com>
2015-06-26 10:53:50 -0700

    Added code comments to ConsumerThreadPool<T>

--- Core/Threading/ConsumerThreadPool`1.cs
+++ Core/Threading/ConsumerThreadPool`1.cs
@@ -6,9 +6,18 @@ using System.Threading.Tasks;
 
 namespace CCSWE.Threading
 {
+    /// <summary>
+    /// Provides a specialized thread pool to process items from a <see cref="BlockingCollection{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
     public class ConsumerThreadPool<T>: IDisposable
     {
         #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsumerThreadPool{T}"/> class specifying the number of consumer threads and the provided function to process items.
+        /// </summary>
+        /// <param name="consumersThreads">The number of consumber threads to create.</param>
+        /// <param name="processItem">The function responsible for processing items added.</param>
         public ConsumerThreadPool(int consumersThreads, Func<T, bool> processItem)
         {
             _items = new BlockingCollection<T>(new ConcurrentQueue<T>());
@@ -33,12 +42,39 @@ namespace CCSWE.Threading
         #endregion
 
         #region Public Properties
+        /// <summary>
+        /// Get the number of items that have failed processing.
+        /// </summary>
         public long ItemsFailed { get { return Interlocked.Read(ref _itemsFailed); } }
+
+        /// <summary>
+        /// Gets the total number of items the have been processed.
+        /// </summary>
         public long ItemsProcessed { get { return Interlocked.Read(ref _itemsProcessed); } }
+
+        /// <summary>
+        /// Gets the number of items that have successfully processed.
+        /// </summary>
         public long ItemsSuccessful { get { return Interlocked.Read(ref _itemsSuccessful); } }
+
+        /// <summary>
+        /// Gets whether this <see cref="ConsumerThreadPool{T}"/> has been marked as complete for adding and is empty.
+        /// </summary>
         public bool IsCompleted { get { return _items.IsCompleted && !_consumerThreadTracker.IsOperationRunning; } }
+
+        /// <summary>
+        /// Gets whether this <see cref="ConsumerThreadPool{T}"/> is continuing to accept items and the <see cref="Progress"/> value is accurate.
+        /// </summary>
         public bool IsIndeterminate { get { return !_items.IsAddingCompleted; } }
+
+        /// <summary>
+        /// Gets the progress of this <see cref="ConsumerThreadPool{T}"/>
+        /// </summary>
         public double Progress { get; protected set; }
+
+        /// <summary>
+        /// Gets the number of items that have been added. 
+        /// </summary>
         public long TotalItems { get { return Interlocked.Read(ref _totalItems); } }
         #endregion
 
@@ -95,6 +131,10 @@ namespace CCSWE.Threading
         #endregion
 
         #region Public Methods
+        /// <summary>
+        /// Adds the item to the <see cref="ConsumerThreadPool{T}"/>.
+        /// </summary>
+        /// <param name="item">Adds the item to the <see cref="ConsumerThreadPool{T}"/>. The item cannot be a null reference.</param>
         public void Add(T item)
         {
             if (item == null)
@@ -107,6 +147,9 @@ namespace CCSWE.Threading
             Interlocked.Increment(ref _totalItems);
         }
 
+        /// <summary>
+        /// Marks the <see cref="ConsumerThreadPool{T}"/> instance as not accepting any more additions.
+        /// </summary>
         public void CompleteAdding()
         {
             _items.CompleteAdding();
@@ -114,6 +157,9 @@ namespace CCSWE.Threading
             //TODO: ConsumerThreadPool<T> - Raise IsAddingComplete and IsIndeterminate property changed?
         }
 
+        /// <summary>
+        /// Releases all resources used by the current instance of the <see cref="ConsumerThreadPool{T}"/> class.
+        /// </summary>
         public void Dispose()
         {
             CompleteAdding();
@@ -122,8 +168,18 @@ namespace CCSWE.Threading
             _items.Dispose();
         }
 
-        public async Task WaitForCompletion()
+        /// <summary>
+        /// Spins until all items have been processed.
+        /// </summary>
+        /// <param name="ignoreIsAddingCompleted">Set this to true if you want to wait in this thread and will call <see cref="CompleteAdding"/> in another thread.</param>
+        /// <returns></returns>
+        public async Task WaitForCompletion(bool ignoreIsAddingCompleted = false)
         {
+            if (!ignoreIsAddingCompleted && !_items.IsAddingCompleted)
+            {
+                throw new InvalidOperationException("Adding is not completed");
+            }
+
             while (!IsCompleted)
             {
                 await Task.Delay(100);
