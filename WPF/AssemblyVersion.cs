405f038e8677ac34a00b311ce80d641d7b758cd0
Cory Charlton <ccharlton@pobox.com>
2015-07-10 12:54:24 -0700

    Enabled T4 generation on build

--- WPF/AssemblyVersion.cs
+++ WPF/AssemblyVersion.cs
@@ -5,5 +5,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.190.8107")]
-[assembly: AssemblyFileVersion("1.0.190.8107")]
+[assembly: AssemblyVersion("1.0.190.8289")]
+[assembly: AssemblyFileVersion("1.0.190.8289")]
